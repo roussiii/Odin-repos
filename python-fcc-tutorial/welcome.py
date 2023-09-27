4d3997962b30e82aa3a6da31361b6e9a66cf3e83
roussiii <rous0308@gmail.com>
2023-09-27 10:35:30 +0400

    create and add FreeCodeCamp python directory to practice python

---
+++ python-fcc-tutorial/welcome.py
@@ -0,0 +1,11 @@
+line01 = "******************"
+line02 = "*                *"
+line03 = "*     WELOME     *"
+
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
